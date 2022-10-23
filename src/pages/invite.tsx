d1b41242f33dcaff909d290ba0e2541192a7425e
nothing521314 <tuan.nguyen5@sotatek.com>
2022-10-23 22:13:26 +0700

    chore: invite form

--- src/pages/invite.tsx
+++ src/pages/invite.tsx
@@ -2,7 +2,7 @@ import ConfettiGenerator from "confetti-js";
 import { Link, navigate } from "gatsby";
 import { useAdminAcceptInvite } from "../../medusa-react";
 import qs from "qs";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { decodeToken } from "react-jwt";
 import Button from "../components/fundamentals/button";
@@ -73,33 +73,36 @@ const InvitePage = ({ location }) => {
   const accept = useAdminAcceptInvite();
   const notification = useNotification();
 
-  const handleAcceptInvite = (data: formValues) => {
-    setPasswordMismatch(false);
+  const handleAcceptInvite = useCallback(
+    (data: formValues) => {
+      setPasswordMismatch(false);
 
-    if (data.password !== data.repeat_password) {
-      setPasswordMismatch(true);
-      return;
-    }
-
-    accept.mutate(
-      {
-        token: parsed.token as string,
-        user: {
-          first_name: data.first_name,
-          last_name: data.last_name,
-          password: data.password,
-        },
-      },
-      {
-        onSuccess: () => {
-          navigate("/login");
-        },
-        onError: (err) => {
-          notification("Error", getErrorMessage(err), "error");
-        },
+      if (data.password !== data.repeat_password) {
+        setPasswordMismatch(true);
+        return;
       }
-    );
-  };
+
+      accept.mutate(
+        {
+          token: parsed.token as string,
+          user: {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            password: data.password,
+          },
+        },
+        {
+          onSuccess: () => {
+            navigate("/login");
+          },
+          onError: (err) => {
+            notification("Error", getErrorMessage(err), "error");
+          },
+        }
+      );
+    },
+    [accept, notification, parsed.token]
+  );
 
   useEffect(() => {
     if (
@@ -114,85 +117,78 @@ const InvitePage = ({ location }) => {
     }
   }, [formState]);
 
-  return (
-    <>
-      {signUp ? (
-        <LoginLayout>
-          <SEO title="Create Account" />
-          <div className="flex h-full w-full items-center justify-center">
-            <div className="flex min-h-[600px] bg-grey-0 rounded-rounded justify-center">
-              <form
-                className="flex flex-col py-12 w-full px-[120px] items-center"
-                onSubmit={handleSubmit(handleAcceptInvite)}
-              >
-                <MedusaIcon />
-                {!token ? (
-                  <div className="h-full flex flex-col gap-y-2 text-center items-center justify-center">
-                    <span className="inter-large-semibold text-grey-90">
-                      You signup link is invalid
-                    </span>
-                    <span className="inter-base-regular mt-2 text-grey-50">
-                      Contact your administrator to obtain a valid signup link
-                    </span>
-                  </div>
-                ) : (
-                  <>
-                    <span className="inter-2xlarge-semibold mt-4 text-grey-90">
-                      Welcome to the team!
-                    </span>
-                    <span className="inter-base-regular text-grey-50 mt-2 mb-large">
-                      Create your account below👇🏼
-                    </span>
-                    <SigninInput
-                      placeholder="First name"
-                      {...register("first_name", { required: true })}
-                      autoComplete="given-name"
-                    />
-                    <SigninInput
-                      placeholder="Last name"
-                      {...register("last_name", { required: true })}
-                      autoComplete="family-name"
-                    />
-                    <SigninInput
-                      placeholder="Password"
-                      type={"password"}
-                      {...register("password", { required: true })}
-                      autoComplete="new-password"
-                    />
-                    <SigninInput
-                      placeholder="Repeat password"
-                      type={"password"}
-                      {...register("repeat_password", { required: true })}
-                      autoComplete="new-password"
-                    />
-                    {passwordMismatch && (
-                      <span className="text-rose-50 w-full mt-2 inter-small-regular">
-                        The two passwords are not the same
-                      </span>
-                    )}
-                    <Button
-                      variant="primary"
-                      size="large"
-                      type="submit"
-                      className="w-full mt-base"
-                      loading={formState.isSubmitting}
-                      disabled={!ready}
-                    >
-                      Create account
-                    </Button>
-                    <Link
-                      to="/login"
-                      className="inter-small-regular text-grey-50 mt-large"
-                    >
-                      Already signed up? Log in
-                    </Link>
-                  </>
-                )}
-              </form>
-            </div>
-          </div>
-        </LoginLayout>
-      ) : (
+  const renderLoginForm = useCallback(() => {
+    if (token) {
+      return (
+        <React.Fragment>
+          <span className="inter-2xlarge-semibold mt-4 text-grey-90">
+            Welcome to the team!
+          </span>
+          <span className="inter-base-regular text-grey-50 mt-2 mb-large">
+            Create your account below👇🏼
+          </span>
+          <SigninInput
+            placeholder="First name"
+            {...register("first_name", { required: true })}
+            autoComplete="given-name"
+          />
+          <SigninInput
+            placeholder="Last name"
+            {...register("last_name", { required: true })}
+            autoComplete="family-name"
+          />
+          <SigninInput
+            placeholder="Password"
+            type={"password"}
+            {...register("password", { required: true })}
+            autoComplete="new-password"
+          />
+          <SigninInput
+            placeholder="Repeat password"
+            type={"password"}
+            {...register("repeat_password", { required: true })}
+            autoComplete="new-password"
+          />
+          {passwordMismatch && (
+            <span className="text-rose-50 w-full mt-2 inter-small-regular">
+              The two passwords are not the same
+            </span>
+          )}
+          <Button
+            variant="primary"
+            size="large"
+            type="submit"
+            className="w-full mt-base"
+            loading={formState.isSubmitting}
+            disabled={!ready}
+          >
+            Create account
+          </Button>
+          <Link
+            to="/login"
+            className="inter-small-regular text-grey-50 mt-large"
+          >
+            Already signed up? Log in
+          </Link>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <div className="h-full flex flex-col gap-y-2 text-center items-center justify-center">
+        <span className="inter-large-semibold text-grey-90">
+          You signup link is invalid
+        </span>
+        <span className="inter-base-regular mt-2 text-grey-50">
+          Contact your administrator to obtain a valid signup link
+        </span>
+      </div>
+    );
+  }, [formState.isSubmitting, passwordMismatch, ready, register, token]);
+
+  const renderBody = useCallback(() => {
+    if (!signUp) {
+      return (
         <div className="bg-grey-90 h-screen w-full overflow-hidden">
           <div className="z-10 flex-grow flex flex-col items-center justify-center h-full absolute inset-0 max-w-[1080px] mx-auto">
             <MedusaVice className="mb-3xlarge" />
@@ -219,9 +215,28 @@ const InvitePage = ({ location }) => {
           </div>
           <canvas id="confetti-canvas" />
         </div>
-      )}
-    </>
-  );
+      );
+    }
+
+    return (
+      <LoginLayout>
+        <SEO title="Create Account" />
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="flex min-h-[600px] bg-grey-0 rounded-rounded justify-center">
+            <form
+              className="flex flex-col py-12 w-full px-[120px] items-center"
+              onSubmit={handleSubmit(handleAcceptInvite)}
+            >
+              <MedusaIcon />
+              {renderLoginForm()}
+            </form>
+          </div>
+        </div>
+      </LoginLayout>
+    );
+  }, [handleAcceptInvite, handleSubmit, renderLoginForm, signUp]);
+
+  return renderBody();
 };
 
 export default InvitePage;
